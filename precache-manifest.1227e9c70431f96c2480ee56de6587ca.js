9abf157fff99005bc6186e84c05bb6d6c829f8d6
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-04-11 16:42:58 -0500

    Updates

---
+++ precache-manifest.1227e9c70431f96c2480ee56de6587ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09efe64edef43083d089b50e35ad8dc8",
+    "url": "/new-shelter/static/media/hero.09efe64e.jpg"
+  },
+  {
+    "revision": "87d132bbec4bdf927059",
+    "url": "/new-shelter/static/js/runtime~main.32649d34.js"
+  },
+  {
+    "revision": "90b957c978428579bf09",
+    "url": "/new-shelter/static/js/main.4083364b.chunk.js"
+  },
+  {
+    "revision": "6a19a321baa72f5c21b1",
+    "url": "/new-shelter/static/js/2.e9783f61.chunk.js"
+  },
+  {
+    "revision": "90b957c978428579bf09",
+    "url": "/new-shelter/static/css/main.0f0a73d7.chunk.css"
+  },
+  {
+    "revision": "9ce5ba38fac4358bd5584d9d07fc5406",
+    "url": "/new-shelter/index.html"
+  }
+];
